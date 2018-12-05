bce3df309d1016b79669bef67bbf30af770524e7
Dan Buda <danbuda@gmail.com>
2018-12-05 14:25:30 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const startBtn = document.getElementById('start');
+const output = document.getElementById('output');
+
+function speak() {
+  const recognition = new webkitSpeechRecognition();
+  recognition.contiuous = true;
+  recognition.interimResults = true;
+  recognition.lang = 'en-BR';
+  recognition.start();
+  
+  recognition.onresult = function(e) {
+    for (let i = e.resultIndex; i < e.results.length; i++) {
+      if (e.results[i].isFinal) {
+        const content = e.results[i][0].transcript.trim();
+        output.textContent = content;
+      }
+    }
+  }
+}
+
+startBtn.addEventListener('click', () => speak());
+
+
